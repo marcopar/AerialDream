99d1cc9da1c346258822ee836c95f913a2b85ea7
marcopar <marcopar@gmail.com>
2021-01-23 22:33:52 +0100

    Readd old video repos also to sh script -- not tested --

--- fetch_playlists.sh
+++ fetch_playlists.sh
@@ -11,7 +11,11 @@ COUNT=`ls -l $RES_PATH/tvos*.json | wc -l`
 # Skip download if files already exist
 if [ $COUNT != 1 ]
 then
+    ### NOT TESTED ON LINUX/MACOS !!! ###
     echo Downloading...
-    wget https://sylvan.apple.com/Aerials/resources-13.tar -qO - --no-check-certificate | tar -xOf - entries.json > $RES_PATH/tvos13.json
+    wget http://a1.phobos.apple.com/us/r1000/000/Features/atv/AutumnResources/videos/entries.json -qO - > $RES_PATH/tvos10.json
+    wget http://sylvan.apple.com/Aerials/2x/entries.json -qO - > $RES_PATH/tvos11.json
+    wget http://sylvan.apple.com/Aerials/resources.tar -qO - | tar -xOf - entries.json > $RES_PATH/tvos12.json
+    wget http://sylvan.apple.com/Aerials/resources-13.tar -qO - | tar -xOf - entries.json > $RES_PATH/tvos13.json
 fi
 
